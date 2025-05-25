463dc9962e9493efe76d583ae5fe718445fda6b2
changhyunryu <cregecko1202@gmail.com>
2025-05-26 08:41:28 +0900

    terraform edit

--- terraform/prod/modules/sg/main.tf
+++ terraform/prod/modules/sg/main.tf
@@ -48,8 +48,8 @@ resource "aws_security_group" "alb_backend" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"]  # 내부 ALB일 경우 내부만 허용
   }
