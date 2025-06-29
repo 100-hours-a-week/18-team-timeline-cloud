acf0ef0c6f02087825c1c26f90da7b4e93bdf109
CreGecko <ckdgusss@gmail.com>
2025-06-29 18:42:20 +0900

    보안 그룹 출력 변수 정리
    - 사용하지 않는 ALB 관련 출력 변수를 제거하여 코드 정리를 진행했습니다.
    - frontend 및 openvpn 보안 그룹 ID 출력 변수는 유지했습니다.

--- terraform/prod/modules/sg/outputs.tf
+++ terraform/prod/modules/sg/outputs.tf
@@ -1,7 +1,3 @@
-# output "alb_sg_id" {
-#   value = aws_security_group.alb.id
-# }
-
 output "frontend_sg_id" {
   value = aws_security_group.frontend.id
 }
@@ -12,14 +8,5 @@ output "backend_sg_id" {
 
 output "openvpn_sg_id" {
   value = aws_security_group.openvpn.id
-}
-
-
-output "sg_alb_frontend_id" {
-  value = aws_security_group.alb_frontend.id
-}
-
-output "sg_alb_backend_id" {
-  value = aws_security_group.alb_backend.id
 }
 
